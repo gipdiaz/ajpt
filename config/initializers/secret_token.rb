c742184538982cb9147d630bc743ee8998266009
Gastón Díaz <gipdiaz@gmail.com>
2013-09-03 19:58:15 -0300

    Inicio del Proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajpt::Application.config.secret_token = 'd121e6cab09d5b3259bede7d5232f3c2663dd8a50befbd2c7f7bdc5bf29812852700370ada33351df91148d24f8f9929882f325c3cb0dfd41bf7ab5c3cb5e191'
